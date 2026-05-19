6308a80098026007f6e56262eac6406ca20f5045
raresamp <rares.ailincai@amplified.software>
2026-05-19 15:12:34 +0300

    Update cloudflared command to use CF_TOKEN

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,5 @@ fi
 rm -rf /home/linuxbrew/.linuxbrew
 ln -sfn /data/.linuxbrew /home/linuxbrew/.linuxbrew
 
-cloudflared tunnel run --token $TOKEN --no-autoupdate &
+cloudflared tunnel run --token $CF_TOKEN --no-autoupdate &
 exec gosu openclaw node src/server.js
